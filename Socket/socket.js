75545efb371f80f091b2ea744eba3a190312136a
Ashish <shawashis500@gmail.com>
2025-05-07 16:42:08 +0530

    changes done

--- Socket/socket.js
+++ Socket/socket.js
@@ -9,13 +9,23 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173/'],
+        origin: ['http://localhost:5173'],
         methods: ['GET', 'POST'],
     },
 });
 
+const userSocketMap = {
+    //
+}
 io.on("connection", (socket) => {
-    console.log("Id : ", socket.id)
+    // console.log("Id : ", socket.id)
+    const userId = socket.handshake.query.userId;
+    // console.log(userId);
+
+    userSocketMap[userId] = socket.id;
+    // console.log(Object.keys(userSocketMap))
+
+    io.emit("onlineUsers", Object.keys(userSocketMap))
 });
 
 module.exports = {
